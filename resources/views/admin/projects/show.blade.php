c8fec2fabbde12fd6e51b08c9f83400b6193fe27
mattialicausi <licausivitomattia@gmail.com>
2023-01-11 23:29:25 +0100

    added project in admin index

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -9,7 +9,7 @@
 
         <div class="row">
             <div class="col-6">
-                <img class="img-fluid" src="{{asset('storage/' .$project->thumb2)}}" alt="image">
+                <img class="img-fluid" src="{{asset('storage/' .$project->thumb1)}}" alt="image">
             </div>
             <div class="col-6 d-flex align-items-center">
                 <p class="text-my-white">{{$project->description}}</p>
@@ -22,7 +22,7 @@
                 <a class="my-btn rounded-pill fw-bold text-my-dark text-decoration-none" href="{{$project->url}}">Explore <span><i class="fa-brands fa-github"></i></span></a>
             </div>
             <div class="col-6">
-                <img class="img-fluid" src="{{asset('storage/' .$project->thumb1)}}" alt="image">
+                <img class="img-fluid" src="{{asset('storage/' .$project->thumb2)}}" alt="image">
             </div>
         </div>
 
