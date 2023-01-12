299e7e97e9876f9a539e48993414a5eead720775
mattialicausi <licausivitomattia@gmail.com>
2023-01-12 15:29:48 +0100

    created migration for add technologies_id into projects table

---
+++ database/migrations/2023_01_12_141725_add_technologies_id_to_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->unsignedBigInteger('technologies_id')->nullable();
+            $table->foreign('technologies_id')->references('id')->on('technologies')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropForeign('projects_technologies_id_foreign');
+            $table->dropColumn('technologies_id');
+        });
+    }
+};
